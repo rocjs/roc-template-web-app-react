4cc68fa932d6af95dd55bdd60a4d18115884eeca
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-04-18 23:11:13 +0200

    Fixed a copy paste bug
    Removed the incorrect information about copyright in the footer.

--- template/app/components/footer/index.js
+++ template/app/components/footer/index.js
@@ -7,7 +7,6 @@ export default () => (
   <div className={styles.footer}>
     <div className="wrapper">
       <a href="http://tech.vg.no" alt="VG Tech"><img className={styles.vg} src={vg} /></a>
-      <span className={styles.info}>Copyright 2016 - <a href="http://tech.vg.no" alt="VG Tech">Verdens Gang AS</a></span>
       <a href="http://github.com/rocjs/roc" alt="Github.com Roc"><img className={styles.gh} src={gh} /></a>
     </div>
   </div>
