a4431200dcb7028739c7552c75a49b2372e18b73
Tom Fagerland <tom@fagerland.org>
2016-05-02 17:23:16 +0200

    Initial commit with tests.

--- template/roc.config.js
+++ template/roc.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   settings: {
     runtime: {
-      applicationName: '{{ rocTitleName }}',
-      port: {{ rocPort }},
+      applicationName: 'Playground',
+      port: 3000,
       serve: ['public', 'build/client'],
       favicon: 'favicon.png',
     },
@@ -11,4 +11,18 @@ module.exports = {
       routes: 'src/routes/index.js',
     },
   },
+ action: () => (rocObject) => {
+    if (rocObject.hook === 'build-webpack') {
+      const externals = {
+        'cheerio': 'window',
+        'react-dom': 'window',
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
+      };
+      return () => () => {
+        rocObject.previousValue.buildConfig.externals = externals;
+        return rocObject.previousValue;
+      };
+    }
+  }
 };
