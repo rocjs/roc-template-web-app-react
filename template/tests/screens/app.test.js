f4ff8f0591687128ba89df5e03e6daf453cf2055
Tom Fagerland <tom@fagerland.org>
2016-05-05 16:31:56 +0200

    Commented tests better, linking to Enzyme docs and explaining a little about each test

--- template/tests/screens/app.test.js
+++ template/tests/screens/app.test.js
@@ -10,9 +10,22 @@ import NavBar from '../../src/components/navbar';
 describe('App screen', () => {
   const empty = [];
   const wrapper = shallow(<App children={ empty } />);
+  /*
+    This test simply uses the wrapper.type() function to get the
+    type of the node, and then compares it with the string 'div'.
+    http://airbnb.io/enzyme/docs/api/ShallowWrapper/type.html
+  */
   it('renders a div', () => {
     expect(wrapper.type()).toEqual('div');
   });
+  /*
+    This test uses wrapper.find() with specific React elements, to check whether
+    those elements exist inside the rendered component. This is a perfect example
+    of unit testing a react component holding other components; everything inside
+    Helment, Footer and NavBar is tested in their own unit tests, so testing this
+    component is really quick and easy.
+    http://airbnb.io/enzyme/docs/api/ShallowWrapper/find.html
+   */
   it('Loads Footer, Navbar and Helmet', () => {
     expect(wrapper.find(Helmet).length).toBeGreaterThan(0);
     expect(wrapper.find(Footer).length).toBeGreaterThan(0);
--- template/tests/screens/start.test.js
+++ template/tests/screens/start.test.js
@@ -7,13 +7,25 @@ import Clicker from '../../src/components/clicker';
 
 describe('Start screen', () => {
   const wrapper = shallow(<Start />);
+  /*
+    This test simply uses the wrapper.type() function to get the
+    type of the node, and then compares it with the string 'div'.
+    http://airbnb.io/enzyme/docs/api/ShallowWrapper/type.html
+  */
   it('renders a div', () => {
     expect(wrapper.type()).toEqual('div');
   });
+  /*
+    This test and the one below use a little trick of Enzymes, \
+    which is that the find() function doesn't just take CSS selectors,
+    but what they call EnzymeSelectors, which can be something as \
+    simple as a React component - or something that renders like it.
+    http://airbnb.io/enzyme/docs/api/ShallowWrapper/find.html
+   */
   it('has a Clicker', () => {
     expect(wrapper.find(Clicker).length).toBeGreaterThan(0);
   });
   it('has an h4 tag', () => {
-    expect(wrapper.find('h4'));
+    expect(wrapper.find(<h4 />));
   });
 });
